64e50f076a4dc55146d0ff1dad5a7bf31cfaee72
Lasse Tjernæs Wardenær <59834731+lassewardenaer@users.noreply.github.com>
2023-12-12 19:50:03 +0100

    Add documentation to Figure3D

--- cydibodi/create_figure.py
+++ cydibodi/create_figure.py
@@ -1,12 +1,38 @@
 import numpy as np
 
 class Figure3D:
+    """
+    A class representing a 3D figure.
+
+    Attributes:
+    - figure: The figure object used for plotting.
+    - ax: The 3D axes object used for plotting.
+
+    Methods:
+    - __init__(self, figure): Initializes the Figure3D object.
+    - draw_cylinder(self, cylinderToCyilinderDistance): Draws cylinders on the plot.
+    - draw_cylinder_lines(self, cordinates_top, cordinates_bottom, c): Draws lines connecting the top and bottom points of the cylinders.
+    - show_axis(self): Shows the x, y, and z axes on the plot.
+    """
+
     def __init__(self, figure):
+        """
+        Initializes the Figure3D object.
+
+        Parameters:
+        - figure: The figure object used for plotting.
+        """
         self.figure = figure
         self.ax = self.figure.add_subplot(111, projection='3d')
         self.ax.set_aspect('equal', adjustable='box')
 
     def draw_cylinder(self, cylinderToCyilinderDistance):
+        """
+        Draws cylinders on the plot.
+
+        Parameters:
+        - cylinderToCyilinderDistance: An object representing the distance between two cylinders.
+        """
         pointsA_top = [cylinderToCyilinderDistance.get_point_cylinder_circle(angle, 'A', 'top')  for angle in np.linspace(0, 2*np.pi, 10)]
         pointsA_bottom = [cylinderToCyilinderDistance.get_point_cylinder_circle(angle, 'A', 'bottom') for angle in np.linspace(0, 2*np.pi, 10)]
         pointsB_top = [cylinderToCyilinderDistance.get_point_cylinder_circle(angle, 'B', 'top') for angle in np.linspace(0, 2*np.pi, 10)]
@@ -44,11 +70,22 @@ class Figure3D:
         self.draw_cylinder_lines(pointsB_top, pointsB_bottom, 'b')
 
     def draw_cylinder_lines(self, cordinates_top, cordinates_bottom, c):
+        """
+        Draws lines connecting the top and bottom points of the cylinders.
+
+        Parameters:
+        - cordinates_top: A list of top points of the cylinders.
+        - cordinates_bottom: A list of bottom points of the cylinders.
+        - c: The color of the lines.
+        """
         for i in range(len(cordinates_top)):
             self.ax.plot([cordinates_top[i][0], cordinates_bottom[i][0]], [cordinates_top[i][1], cordinates_bottom[i][1]], [cordinates_top[i][2], cordinates_bottom[i][2]], c=c)
 
 
     def show_axis(self):
+        """
+        Shows the x, y, and z axes on the plot.
+        """
         self.ax.quiver(0, 0, 0, 1, 0, 0, length=1, normalize=True, color='k')
         self.ax.quiver(0, 0, 0, 0, 1, 0, length=1, normalize=True, color='r')
         self.ax.quiver(0, 0, 0, 0, 0, 1, length=1, normalize=True, color='b')
